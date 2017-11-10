460115479104ce1c320747277534a0b59f136d0c
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-11 00:44:41 +0200

    Add mixin example with events

---
+++ JavaScript/6-events.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const emitable = obj => Object.assign(obj, {
+  events: {},
+  on(name, fn) {
+    const event = this.events[name] || [];
+    this.events[name] = event;
+    event.push(fn);
+  },
+  emit(name, ...data) {
+    const event = this.events[name];
+    if (event) event.forEach(fn => fn(...data));
+  }
+});
+
+const movable = obj => {
+  obj.on('move', (x, y) => {
+    console.log('move', x, y);
+    obj.x += x;
+    obj.y += y;
+    obj.emit('moved');
+  });
+  return obj;
+};
+
+// Usage
+
+const Rect = class {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  toString() {
+    return `[${this.x}, ${this.y}, ${this.width}, ${this.height}]`;
+  }
+};
+
+const r1 = movable(emitable(new Rect(10, 20, 50, 150)));
+console.log(r1.toString());
+r1.on('moved', () => {
+  console.log(r1.toString());
+});
+r1.emit('move', 10, 20);
