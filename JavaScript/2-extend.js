063f5fc4043b749460ea6591b33dc20d9653a212
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-10 22:24:41 +0200

    Update examples

--- JavaScript/2-extend.js
+++ JavaScript/2-extend.js
@@ -1,25 +1,28 @@
 'use strict';
 
-/*
-function extend(obj, mixin) {
+/*function extend(obj, mixin) {
   for (let key in mixin) {
     if (mixin.hasOwnProperty(key)) {
       obj[key] = mixin[key];
     }
   }
   return obj;
-}
-*/
+}*/
 
-const extend = (obj, mixin) => (
-  Object.keys(mixin).forEach(key => obj[key] = mixin[key]), obj
-);
+const extend = (obj, mixin) => {
+  Object.keys(mixin).forEach(key => obj[key] = mixin[key]);
+  return obj;
+};
 
-const obj1 = { name: 'Marcus Aurelius', city: 'Rome', born: '121-04-26' };
+const obj1 = {
+  name: 'Marcus Aurelius',
+  city: 'Rome',
+  born: '121-04-26'
+};
 
 const mix1 = {
   toString() {
-    return this.name + ' was born in ' + this.city + ' in ' + this.born;
+    return `${this.name} was born in ${this.city} in ${this.born}`;
   },
   age() {
     const year = new Date().getFullYear();
@@ -31,4 +34,4 @@ const mix1 = {
 extend(obj1, mix1);
 console.log(obj1);
 console.log(obj1.toString());
-console.log('His age is ' + obj1.age() + ' as of today');
+console.log(`His age is ${obj1.age()} as of today`);
--- JavaScript/3-assign.js
+++ JavaScript/3-assign.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const obj1 = { name: 'Marcus Aurelius', city: 'Rome', born: '121-04-26' };
+const obj1 = {
+  name: 'Marcus Aurelius',
+  city: 'Rome',
+  born: '121-04-26'
+};
 
 const mix1 = {
   toString() {
-    return this.name + ' was born in ' + this.city + ' in ' + this.born;
+    return `${this.name} was born in ${this.city} in ${this.born}`;
   },
   age() {
     const year = new Date().getFullYear();
@@ -16,4 +20,4 @@ const mix1 = {
 Object.assign(obj1, mix1);
 console.log(obj1);
 console.log(obj1.toString());
-console.log('His age is ' + obj1.age() + ' as of today');
+console.log(`His age is ${obj1.age()} as of today`);
